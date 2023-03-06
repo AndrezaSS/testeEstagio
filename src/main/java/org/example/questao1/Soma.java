72bfb690c9ab61696388c46d5a39cf3d4bab15cd
Andreza Santos <108132741+AndrezaSS@users.noreply.github.com>
2023-03-06 15:35:39 -0300

    Update Soma.java

--- src/main/java/org/example/questao1/Soma.java
+++ src/main/java/org/example/questao1/Soma.java
@@ -1,2 +1,15 @@
 package org.example.questao1;public class Soma {
+  public static void main(String[] args) {
+        int indice = 13;
+        int soma = 0;
+
+
+        for (int k = 0; k < indice ; k++) {
+            soma = soma + k;
+            System.out.println(soma);
+//A soma é 78
+
+        }
+    }
 }
+

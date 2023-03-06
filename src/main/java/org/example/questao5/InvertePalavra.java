5212b5df0101d51f2bb9dac37be605ff728c2b6f
Andreza Santos <108132741+AndrezaSS@users.noreply.github.com>
2023-03-06 15:36:23 -0300

    Update InvertePalavra.java

--- src/main/java/org/example/questao5/InvertePalavra.java
+++ src/main/java/org/example/questao5/InvertePalavra.java
@@ -1,2 +1,16 @@
 package org.example.questao5;public class InvertePalavra {
+  public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite uma palavra: ");
+        String x = teclado.nextLine();
+        //Definimos essa variável para armazenar os caracteres da variável x
+        String palavraInvertida = "";
+
+        for (int i = x.length() - 1; i >= 0; i--) {
+            palavraInvertida += x.charAt(i);
+        }
+
+        System.out.println("Palavra invertida: " + palavraInvertida);
+    }
 }
+
